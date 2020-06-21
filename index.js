3be2d7ab9eb8d58d32595d90e2f0749595e53756
Nianyi Wang <WangNianyi2001@outlook.com>
2020-06-22 07:32:05 +0800

    Implement Histogram

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const article = document.getElementsByTagName('article')[0];
 const live = document.getElementById('live');
 const distribution = document.getElementById('distribution');
 const
-	distribution_size = distribution.width = distribution.height = 128,
-	half_ds = distribution_size / 2;
+	granularity = distribution.width = distribution.height = 256,
+	half_granularity = granularity / 2;
 const _distribution = distribution.getContext('2d');
 
 const image = new Image();
@@ -26,69 +26,127 @@ async function downscaleTo(source, destination) {
 
 const computeLuminance = (r, g, b) => 1.26e-3 * r + 1.89e-3 * g + 7.56e-4 * b;
 const TWO_OVER_SQRT3 = 1.1547;
-const distributionByLuminance = (r, g, b) => {
-	const luminance = computeLuminance(r, g, b);
-	if(r === g && g === b)
-		return [half_ds, half_ds * (1 - luminance)];
+const distributeBySaturation = (r, g, b) => {
 	const raw = [(b - g) * TWO_OVER_SQRT3, (b + g) / 2 - r];
-	const module = Math.sqrt(raw[0] * raw[0] + raw[1] * raw[1]);
-	const k = half_ds * Math.pow(luminance, 3) / module;
+	const k = half_granularity / 256;
 	return [
-		Math.floor(half_ds + raw[0] * k),
-		Math.floor(half_ds + raw[1] * k)
+		Math.floor(half_granularity + raw[0] * k),
+		Math.floor(half_granularity + raw[1] * k)
 	];
 };
-const distributionBySaturation = (r, g, b) => {
+const distributeByLuminance = (r, g, b) => {
+	const luminance = computeLuminance(r, g, b);
+	if(r === g && g === b)
+		return;
 	const raw = [(b - g) * TWO_OVER_SQRT3, (b + g) / 2 - r];
-	const k = half_ds / 256;
+	const module = Math.sqrt(raw[0] * raw[0] + raw[1] * raw[1]);
+	const k = half_granularity * Math.pow(luminance, 3) / module;
 	return [
-		Math.floor(half_ds + raw[0] * k),
-		Math.floor(half_ds + raw[1] * k)
+		Math.floor(half_granularity + raw[0] * k),
+		Math.floor(half_granularity + raw[1] * k)
 	];
 };
 let distribution_mode = 'Luminance';
+const histogram_vertical_amplify = 3;
+const histogram_color_sequence = {
+	'rgb': ['red', 'yellow', 'white'],
+	'rbg': ['red', 'purple', 'white'],
+	'grb': ['green', 'yellow', 'white'],
+	'gbr': ['green', 'cyan', 'white'],
+	'brg': ['blue', 'purple', 'white'],
+	'bgr': ['blue', 'cyan', 'white']
+};
 const distribution_modes = {
 	'Saturation': {
 		name: 'Saturation',
-		projection: distributionBySaturation,
+		distribute(data, opacity) {
+			for(let i = 0; i < data.length; i += 4) {
+				const [r, g, b] = [
+					data[i],
+					data[i + 1],
+					data[i + 2],
+				];
+				_distribution.beginPath();
+				_distribution.rect(...distributeBySaturation(r, g, b), 1, 1);
+				_distribution.fillStyle = `rgba(${r},${g},${b},${opacity})`;
+				_distribution.fill();
+			}
+		},
 		background: 'icon/RGB-background.svg'
 	},
 	'Luminance': {
 		name: 'Luminance',
-		projection: distributionByLuminance,
+		distribute(data, opacity) {
+			for(let i = 0; i < data.length; i += 4) {
+				const [r, g, b] = [
+					data[i],
+					data[i + 1],
+					data[i + 2],
+				];
+				_distribution.beginPath();
+				const coordinate = distributeByLuminance(r, g, b);
+				if(!coordinate)
+					continue;
+				_distribution.rect(...coordinate, 1, 1);
+				_distribution.fillStyle = `rgba(${r},${g},${b},${opacity})`;
+				_distribution.fill();
+			}
+		},
 		background: 'icon/RGB-background.svg'
 	},
 	'Histogram': {
 		name: 'Histogram',
-		projection: distributionByLuminance,
+		distribute(data, opacity) {
+			opacity *= histogram_vertical_amplify;
+			const [br, bg, bb, bl] =
+				Array(4).fill(0).map(_ => Array(granularity).fill(0));
+			for(let i = 0; i < data.length; i += 4) {
+				const [r, g, b] = [
+					data[i],
+					data[i + 1],
+					data[i + 2]
+				];
+				const luminance = computeLuminance(r, g, b);
+				br[Math.floor(luminance * r)] += opacity;
+				bg[Math.floor(luminance * g)] += opacity;
+				bb[Math.floor(luminance * b)] += opacity;
+				bl[Math.floor(luminance * granularity)] += opacity;
+			}
+			_distribution.lineWidth = 1;
+			for(let i = 0; i < granularity; ++i) {
+				const column = [
+					['r', br[i]],
+					['g', bg[i]],
+					['b', bb[i]]
+				].sort((a, b) => b[1] - a[1]);
+				const color_sequence = histogram_color_sequence[
+					column[0][0] + column[1][0] + column[2][0]
+				];
+				const y = [column[0][1], column[1][1], column[2][1], 0];
+				for(let j = 0; j < 3; ++j) {
+					_distribution.beginPath();
+					_distribution.moveTo(i, granularity - y[j]);
+					_distribution.lineTo(i, granularity - y[j + 1]);
+					_distribution.strokeStyle = color_sequence[j];
+					_distribution.stroke();
+				}
+			}
+		},
 		background: 'icon/histogram-background.svg'
 	},
 };
-async function renderDistribution(source, downscale_level = 5, saturate_portion = 2e3) {
-	const projection = distribution_modes[distribution_mode].projection;
+async function renderDistribution(source, downscale_level = 5, opacity_k = 2e3) {
+	_distribution.clearRect(0, 0, granularity, granularity);
 	const canvas = document.createElement('canvas');
-	const [w, h]
-		= [canvas.width, canvas.height]
-		= [Math.ceil(source.width / downscale_level), Math.ceil(source.height / downscale_level)];
+	const [w, h] = [canvas.width, canvas.height] = [
+		Math.ceil(source.width / downscale_level),
+		Math.ceil(source.height / downscale_level)
+	];
 	await downscaleTo(source, canvas);
-	const context = canvas.getContext('2d');
-	const opacity = saturate_portion / w / h;
-	const data = context.getImageData(0, 0, w, h).data;
-	_distribution.clearRect(0, 0, distribution_size, distribution_size);
-	for(let y = 0; y < h; ++y) {
-		for(let x = 0; x < w; ++x) {
-			const i = (y * h + x) << 2;
-			const [r, g, b] = [
-				data[i],
-				data[i + 1],
-				data[i + 2],
-			];
-			_distribution.beginPath();
-			_distribution.rect(...projection(r, g, b), 1, 1);
-			_distribution.fillStyle = `rgba(${r},${g},${b},${opacity})`;
-			_distribution.fill();
-		}
-	}
+	distribution_modes[distribution_mode].distribute(
+		canvas.getContext('2d').getImageData(0, 0, w, h).data,
+		opacity_k / w / h
+	);
 }
 function changeDistributionMode(mode_name) {
 	if(mode_name === distribution_mode.name)
