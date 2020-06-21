0217f6fb498045dbaaf1290ec535f71c8470bace
Nianyi Wang <WangNianyi2001@outlook.com>
2020-06-21 23:36:37 +0800

    Basic layout

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const $open = document.getElementById('open');
+
+let image = new Image();
+function load($) {
+	if(!$.files)
+		return;
+	const reader = new FileReader();
+	reader.onload = e => image.src = e.target.result;
+	reader.readAsDataURL($.files[0]);
+}
+image.onload = function postLoad() {
+	//
+};
+function forceLoad() {
+	const e = document.createEvent('MouseEvents');
+	e.initEvent('click', true, false);
+	$open.dispatchEvent(e);
+}
+window.addEventListener('keydown', e => {
+	if(e.key === 'o' && e.ctrlKey) {
+		e.preventDefault();
+		forceLoad();
+	}
+});
